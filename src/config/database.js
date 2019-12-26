a76010e04f3b769be4aa2fb95bd98e355328aad6
skynetsistemas <skynetsistemas@lojacasasaoluiz.com.br>
2019-12-25 21:55:10 -0300

    Criação do DB

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'asodre',
+  database: 'gobarber2',
+  define: {
+    timestamps: true,
+    underscored: true,
+    uderscoredAll: true,
+  },
+};
