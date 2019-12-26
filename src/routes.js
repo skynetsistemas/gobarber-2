5105054162c9e30f0d599c0b6bbede0594f1b393
skynetsistemas <skynetsistemas@lojacasasaoluiz.com.br>
2019-12-25 22:13:11 -0300

    Criação de Usuários

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
- 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Alex Sodré' });
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Alex Sodré',
+    email: 'alex.sodre@lojacasasaoluiz.com.br',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 })
 
 export default routes;
