b26121014cd31e3066ef4ae5eb506aff02b7eb10
skynetsistemas <skynetsistemas@lojacasasaoluiz.com.br>
2019-12-27 09:25:29 -0300

    Autenticação do Token

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,6 +20,14 @@ class UserController {
 
   }
 
+  async update(req, res) {
+
+    console.log(req.userId);
+
+    return res.json({ ok: true });
+
+  }
+
 }
 
 export default new UserController();
