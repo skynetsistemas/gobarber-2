73642a7f363e00f226e81042d79afb068297f7fd
skynetsistemas <skynetsistemas@lojacasasaoluiz.com.br>
2019-12-31 16:21:20 -0300

    salvando alterações

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -10,11 +10,11 @@ class SessionController {
 
     const user = await User.findOne({ where: { email } });
 
-    if(!user) {
+    if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    if(!(await user.checkPassword(password))) {
+    if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match' });
 
 
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,12 +20,9 @@ class UserController {
 
   }
 
+
   async update(req, res) {
-
-    console.log(req.userId);
-
     return res.json({ ok: true });
-
   }
 
 }
